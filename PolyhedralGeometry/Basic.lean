2138e970b9be26f36a2a19edf386d5abc884fa4b
Caelan Ritter <caelanritter@gmail.com>
2025-02-03 17:05:31 -0800

    Define polyhedron; prove halfspace is convex

--- PolyhedralGeometry/Basic.lean
+++ PolyhedralGeometry/Basic.lean
@@ -13,15 +13,42 @@ def Halfspace (s : Set V) : Prop :=
   -- "there exists a linear functional f and a constant c such that s equals the set of all points x in V such that f(x) ≤ c"
   ∃ (f : V →ₗ[ℝ] ℝ) (c : ℝ), s = { x | f x ≤ c }
 
---define polyhedron (we need a notion of finite set/type for this!)
-def Polyhedron (s : Set V) : Prop := sorry
+def Polyhedron (s : Set V) : Prop :=
+  ∃ (I : Type*) (H : I → Set V), Finite I ∧ (∀ i : I, Halfspace (H i)) ∧ s = ⋂ (i : I), H i
 
---show that a halfspace is convex
 lemma halfspace_convex : ∀ (s : Set V), Halfspace s → Convex ℝ s := by
-  sorry
+  intro s h_s_halfspace
+  unfold Convex
+  intro x h_x_in_s
+  unfold StarConvex
+  intro y h_y_in_s a b h_a_nonneg h_b_nonneg h_a_b_one
+  show a • x + b • y ∈ s
+  unfold Halfspace at h_s_halfspace
+  rcases h_s_halfspace with ⟨f, ⟨c, rfl⟩⟩
+  -- rw [Set.mem_def] at h_x_in_s
+  -- dsimp at h_x_in_s -- doesn't work!
+  have h_x_in_s : f x ≤ c := by assumption
+  have h_y_in_s : f y ≤ c := by assumption
+  show f (a • x + b • y) ≤ c
+  calc
+    f (a • x + b • y) = f (a • x) + f (b • y) := by
+      apply LinearMap.map_add
+    _ = a * f x + b * f y := by
+      repeat rw [LinearMap.map_smul]
+      rfl
+    _ ≤ a * c + b * c := by
+      apply add_le_add
+      <;> apply mul_le_mul_of_nonneg_left
+      <;> assumption
+    _ = (a + b) * c := by rw [add_mul]
+    _ = 1 * c := by rw [h_a_b_one]
+    _ = c := one_mul c
 
---show that a polyhedron is convex
+--use this instead!
+#check convex_halfSpace_le
+
 theorem poly_convex : ∀ (s : Set V), Polyhedron s → Convex ℝ s := by
+  --to do it the easy way, use `convex_iInter`
   sorry
 
 --todo:
@@ -29,6 +56,9 @@ theorem poly_convex : ∀ (s : Set V), Polyhedron s → Convex ℝ s := by
 
 
 
+
+--todo:
+--define polytope (needs notion of boundedness; show that it doesn't depend on the choice of inner product, so the definition can be made without such a choice)
 
 
 
