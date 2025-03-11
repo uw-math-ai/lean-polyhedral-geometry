96623ce68e7e3bdb30d8586d51df1b9d4022ad5b
Caelan Ritter <caelanritter@gmail.com>
2025-03-10 23:41:37 -0700

    Caratheodory - proof outline with `sorry`s

--- PolyhedralGeometry/Basic.lean
+++ PolyhedralGeometry/Basic.lean
@@ -38,8 +38,6 @@ lemma halfspace_convex : ∀ (s : Set V), Halfspace s → Convex ℝ s := by
     _ = (a + b) * c := by rw [add_mul]
     _ = 1 * c := by rw [h_a_b_one]
     _ = c := one_mul c
---use this instead!
-#check convex_halfSpace_le
 
 theorem poly_convex : ∀ (s : Set V), Polyhedron s → Convex ℝ s := by
   intro s h_s_poly
@@ -91,7 +89,7 @@ example (s : Set V) (f : V →ₗ[ℝ] ℝ) (c : ℝ) : Cone s → (∀ x ∈ s,
     show False
     linarith
 
-def conicalCombo_cards (s : Set V) (x : V) : Set ℕ := Finset.card '' { (t : Finset V) | ∃ (a : V → ℝ), (∀ v ∈ t, 0 ≤ a v) ∧ ↑t ⊆ s ∧ x = ∑ v in t, a v • v}
+def conicalCombo_cards (s : Set V) (x : V) : Set ℕ := Finset.card '' { (t : Finset V) | ∃ (a : V → ℝ), (∀ v ∈ t, 0 ≤ a v) ∧ ↑t ⊆ s ∧ x = ∑ v ∈ t, a v • v}
 
 lemma conicalCombo_cards_nonempty (s : Set V) (x : V) : x ∈ conicalHull s → (conicalCombo_cards s x).Nonempty := by
   intro ⟨vectors,h⟩
@@ -115,6 +113,10 @@ variable [FiniteDimensional ℝ V]
 
 open Classical
 
+section
+
+variable (α β : Type*) [AddCommMonoid β]
+
 -- theorem 1.3.2(b)
 theorem caratheordory (s : Set V) (x : V) (h : x ∈ conicalHull s) :
   ∃ (t : Finset V), ↑t ⊆ s ∧ t.card ≤ Module.finrank ℝ V ∧ x ∈ conicalHull t := by
@@ -124,40 +126,88 @@ theorem caratheordory (s : Set V) (x : V) (h : x ∈ conicalHull s) :
   -- induction' u using Finset.induction_on with v u h_v_nin_u ih
   -- . sorry
   -- . sorry
-  rcases min_elt (conicalCombo_cards s x) (conicalCombo_cards_nonempty s x h) with ⟨n, h₁, h₂⟩
-  rcases h₁ with ⟨t, ⟨a, h_a_nonneg, h_t_subset, h_x_combo⟩, rfl⟩
+  rcases min_elt (conicalCombo_cards s x) (conicalCombo_cards_nonempty s x h) with ⟨n, h', h_minimality⟩
+  rcases h' with ⟨t, ⟨a, h_a_nonneg, h_t_subset, h_x_combo⟩, rfl⟩
   rcases le_or_gt t.card (Module.finrank ℝ V) with h_t_card | h_t_card
   . use t, h_t_subset, h_t_card, t, a
   apply False.elim
-  have h_not_lin_indep : ¬(LinearIndependent ℝ (fun x => x : ↑t → V)) := by
+  have h_not_lin_indep : ¬(LinearIndependent ℝ (fun x => x : {x // x ∈ t} → V)) := by
     intro h
-    have h₁ := LinearIndependent.cardinal_le_rank h
-    have := Cardinal.toNat_le_toNat h₁ (Module.rank_lt_aleph0 ℝ V)
+    have := LinearIndependent.cardinal_le_rank h
+    have := Cardinal.toNat_le_toNat this (Module.rank_lt_aleph0 ℝ V)
     simp at this
     linarith!
   have := Fintype.not_linearIndependent_iff.mp h_not_lin_indep
-  rcases this with ⟨b, h_combo, u, h_b_u_ne_0⟩
+  rcases this with ⟨b, h_b_combo, ⟨u, h_u_t⟩, h_b_u_ne_0⟩
   let b' : V → ℝ := fun v =>
     if hvt : v ∈ t then b { val := v, property := hvt} else 0
-  --have h_combo₁ : ∑ v ∈ t, b' v = 0 := sorry
+  have h_b'_u_ne_0 : b' u ≠ 0 := by simp [b']; use h_u_t
+  have h_b'_combo : ∑ v ∈ t, b' v • v = 0 := by
+    simp [b']
+    rw [←h_b_combo, Finset.sum_dite_of_true]
+    simp
   by_cases h' : b' u > 0
-  #check { x // x ∈ t}
   . let ratio : V → ℝ := fun i => (b' i) / (a i)
-    have : {x | x ∈ t}.Nonempty := by
+    have h_t_nonempty : {x | x ∈ t}.Nonempty := by
       apply Set.nonempty_of_ncard_ne_zero
       have : t.card > 0 := by linarith
       show (↑t : Set V).ncard ≠ 0
       rw [Set.ncard_coe_Finset]
       linarith
-    have := Set.exists_max_image {x | x ∈ t} ratio (Set.finite_mem_finset t) this
+    have := Set.exists_max_image {x | x ∈ t} ratio (Set.finite_mem_finset t) h_t_nonempty
     rcases this with ⟨u', h_u'_t, h_u'_max⟩
-    dsimp at h_u'_t
     simp [ratio] at h_u'_max
+    let α := a u' / b' u'
+    have h_b'_u'_ne_0 : b' u' ≠ 0 := by sorry
+    have h₁ : (a - α • b') u' = 0 := by sorry
+    have h₂ : ∀ v ∈ t, (a - α • b') v ≥ 0 := by sorry
+    have h_x_combo' : x = ∑ v ∈ t, (a - α • b') v • v := by sorry
+    have : t.card - 1 ∈ conicalCombo_cards s x := by
+      have h' : {x | x ∈ t ∧ x ≠ u'} ⊆ t := by sorry
+      have : {x | x ∈ t ∧ x ≠ u'}.Finite := Set.Finite.subset (by sorry : (t.toSet).Finite) h'
+      let t' : Finset V := Set.Finite.toFinset this
+      use t'
+      have h_t'_ss_t : t' ⊆ t := by simp [t']; exact h'
+      have t'_def : t = {u'} ∪ t' := by
+        ext v
+        constructor
+        . intro _
+          by_cases h' : v = u'
+          . rw [h']
+            apply Finset.mem_union_left
+            apply Finset.mem_singleton_self
+          . apply Finset.mem_union_right
+            simp [t']
+            constructor <;> assumption
+        . intro h
+          rw [Finset.mem_union] at h
+          rcases h with h | h
+          . have : v = u' := Finset.mem_singleton.mp h
+            rw [this]
+            assumption
+          . simp [t'] at h
+            exact h.left
+      constructor
+      . use a - α • b'
+        constructor
+        . sorry
+        . constructor
+          . sorry
+          . sorry
+      . apply Nat.eq_sub_of_add_eq
+        apply Eq.symm
+        rw [t'_def]
+        calc
+          ({u'} ∪ t').card = ({u'} ∪ t').card + ({u'} ∩ t').card := by sorry
+          _ = ({u'} : Finset V).card + t'.card := by apply Finset.card_union_add_card_inter
+          _ = 1 + t'.card := by congr
+          _ = t'.card + 1 := add_comm _ _
+    have : t.card - 1 < t.card := by sorry -- this is not trivial since 0 - 1 = 0 in Nat (so linarith can't solve it without some help)
+    have := h_minimality (t.card - 1) this
+    contradiction
+  . let b' : V → ℝ := fun i => -b' i
+    --there should be a way to redefine b' above selectively given this condition, so that we don't have to repeat the proof above verbatim
     sorry
-  . let b_neg : { x // x ∈ t } → ℝ := fun i => -(b i)
-    sorry
-
-#check not_linearIndependent_iff
 
 variable {ι : Type*} [Finite ι] (B : Basis ι ℝ V)
 
